6858399f30122d5bd3386e366aec62a4189d7690
andrew.bandura@gmail.com <a1n9d7y7>
2018-04-02 07:58:10 +0300

    -init

--- src/main/java/com/app/UserDaoImpl.java
+++ src/main/java/com/app/UserDaoImpl.java
@@ -1,9 +1,13 @@
 package com.app;
 
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDaoImpl implements UserDao {
+    public UserDaoImpl() {
+    }
+
     public String getName() {
         return "Bill Gates";
     }
--- src/main/java/com/app/UserServlet.java
+++ src/main/java/com/app/UserServlet.java
@@ -1,6 +1,8 @@
 package com.app;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,14 +10,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class UserServlet extends HttpServlet {
 
+    static final Logger LOGGER  = Logger.getLogger(UserServlet.class);
+
     @Autowired
-    UserDao userDao;
+    private UserDao userDao;
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public UserServlet() {
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        LOGGER.info("This is a logging statement from log4j");
+
+//        String html = "<html><h2>Log4j has been initialized successfully!</h2></html>";
+//        resp.getWriter().println(html);
         super.doGet(req, resp);
     }
 }
